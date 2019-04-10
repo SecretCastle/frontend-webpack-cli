27cfccb7c518476c6e0dd6004fcdcbc1b1219dc9
SecretCastle <chenfangzhong@hotmail.com>
2019-04-10 22:26:15 +0800

    upgrade config

--- build/webpack.server.js
+++ build/webpack.server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const webpack = require('webpack');
 const WebpackDevMiddleware = require('webpack-dev-middleware');
 const app = express();
-const argv = require('yargs').argv;
-const NODE_ENV = argv.env;
+const Config = require('../config');
 
 // 加载webpack配置文件
 const config = require('./webpack.dev');
 
 // 如果环境是test，则webpack的mode为none
-if (NODE_ENV === 'test') {
+if (Config.NODE_ENV === 'test') {
 	config.mode = 'none';
 }
 
