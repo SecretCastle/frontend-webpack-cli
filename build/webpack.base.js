3177f91365426fb757d614b725c1933f2448a8d8
SecretCastle <chenfangzhong@hotmail.com>
2019-04-16 20:28:47 +0800

    upgrade editor

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -6,125 +6,125 @@ const Config = require('../config');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const webpackBasicConfig = {
-	context: Config.contextPath,
-	output: {
-		filename: 'js/[name].[hash].bundle.js',
-		chunkFilename: 'js/[name].[hash].bundle.js',
-		path: path.resolve(__dirname, '..', Config.outputFolderName),
-		publicPath: Config.NODE_ENV === 'prod' ? Config.build.publicPath : Config.development.publicPath
-	},
-	// 解析
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, '../src')
-		},
-		extensions: ['.js', '.jsx', '.vue', '.css', '.less', '.json', '.ejs', '.png', '.jpg', '.jpeg', '.gif'],
-		modules: ['./src', 'node_modules']
-	},
-	plugins: [
-		new VueLoaderPlugin(),
-		// 清除打包输出的文件夹
-		new CleanWebpackPlugins({
-			cleanOnceBeforeBuildPatterns: path.join(process.cwd(), `${Config.outputFolderName}/**/*`)
-		}),
-		// manifest
-		new ManifestPlugin(),	
-	],
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: [
-					'babel-loader',
-					'eslint-loader'
-				]
-			},
-			// 不处理node_modules中的css, 使用node_modules
-			{
-				test: /\.css$/,
-				loader: [
-					Config.NODE_ENV === 'prod' 
-						?  MiniCssExtractPlugin.loader 
-						: 'vue-style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: '[hash:base64:6]',
-							sourceMap: Config.enableCSSSourceMap ? true : false
-						}
-					},
-					'postcss-loader'
-				],
-				// 不打包文件
-				exclude: [path.resolve(__dirname, '..', 'node_modules')]
-			},
-			// 单独处理node_modules，不使用node_modules
-			{
-				test: /\.css$/,
-				loader: [
-					Config.NODE_ENV === 'prod' || Config.NODE_ENV === 'test' 
-						?  MiniCssExtractPlugin.loader 
-						: 'vue-style-loader',
-					'css-loader',
-					'postcss-loader'
-				],
-				include: [path.resolve(__dirname, '..', 'node_modules')]
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: [
-					{
-						loader: 'file-loader',
-						options: {
-							outputPath: `${Config[Config.NODE_ENV === 'prod' ? 'build' : 'development'].assetsFolderPath}/font`
-						}
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				loader: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 10000, // 10kb以下文件，使用data64形式输出
-							outputPath: `${Config[Config.NODE_ENV === 'prod' ? 'build' : 'development'].assetsFolderPath}images`
-						}
-					}
-				]
-			},
-			{
-				test: /\.less/,
-				loader: [
-					Config.NODE_ENV === 'prod' || Config.NODE_ENV === 'test' 
-						?  MiniCssExtractPlugin.loader 
-						: 'vue-style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: '[hash:base64:6]',
-							sourceMap: Config.enableCSSSourceMap ? true : false
-						}
-					},
-					'postcss-loader',
-					{
-						loader: 'less-loader',
-						options: {
-							sourceMap: Config.enableCSSSourceMap ? true : false
-						}
-					}
-				],
-				exclude: [path.resolve(__dirname, '..', 'node_modules')]
-			}
-		]
-	}
+    context: Config.contextPath,
+    output: {
+        filename: 'js/[name].[hash].bundle.js',
+        chunkFilename: 'js/[name].[hash].bundle.js',
+        path: path.resolve(__dirname, '..', Config.outputFolderName),
+        publicPath: Config.NODE_ENV === 'prod' ? Config.build.publicPath : Config.development.publicPath
+    },
+    // 解析
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '../src')
+        },
+        extensions: ['.js', '.jsx', '.vue', '.css', '.less', '.json', '.ejs', '.png', '.jpg', '.jpeg', '.gif'],
+        modules: ['./src', 'node_modules']
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        // 清除打包输出的文件夹
+        new CleanWebpackPlugins({
+            cleanOnceBeforeBuildPatterns: path.join(process.cwd(), `${Config.outputFolderName}/**/*`)
+        }),
+        // manifest
+        new ManifestPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: [
+                    'babel-loader',
+                    'eslint-loader'
+                ]
+            },
+            // 不处理node_modules中的css, 使用node_modules
+            {
+                test: /\.css$/,
+                loader: [
+                    Config.NODE_ENV === 'prod'
+                        ? MiniCssExtractPlugin.loader
+                        : 'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[hash:base64:6]',
+                            sourceMap: Config.enableCSSSourceMap ? true : false
+                        }
+                    },
+                    'postcss-loader'
+                ],
+                // 不打包文件
+                exclude: [path.resolve(__dirname, '..', 'node_modules')]
+            },
+            // 单独处理node_modules，不使用node_modules
+            {
+                test: /\.css$/,
+                loader: [
+                    Config.NODE_ENV === 'prod' || Config.NODE_ENV === 'test'
+                        ? MiniCssExtractPlugin.loader
+                        : 'vue-style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ],
+                include: [path.resolve(__dirname, '..', 'node_modules')]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: `${Config[Config.NODE_ENV === 'prod' ? 'build' : 'development'].assetsFolderPath}/font`
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000, // 10kb以下文件，使用data64形式输出
+                            outputPath: `${Config[Config.NODE_ENV === 'prod' ? 'build' : 'development'].assetsFolderPath}images`
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less/,
+                loader: [
+                    Config.NODE_ENV === 'prod' || Config.NODE_ENV === 'test'
+                        ? MiniCssExtractPlugin.loader
+                        : 'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[hash:base64:6]',
+                            sourceMap: Config.enableCSSSourceMap ? true : false
+                        }
+                    },
+                    'postcss-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            sourceMap: Config.enableCSSSourceMap ? true : false
+                        }
+                    }
+                ],
+                exclude: [path.resolve(__dirname, '..', 'node_modules')]
+            }
+        ]
+    }
 };
 
 module.exports = webpackBasicConfig;
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -15,91 +15,91 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const webpackConfig = merge(baseConfig, {
-	mode: 'production',
-	entry: Utils.getEntry(),
-	plugins: [
-		// 设置当前环境为production
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'assets/css/[name].[hash].css',
-			chunkFilename: 'assets/css/[id].[hash].css',
-			hmr: Config.NODE_ENV === 'dev',
-			publicPath: Config.build.publicPath
-		}),
-		// 该插件会根据模块的相对路径生成一个四位数的hash作为模块id, 保证vendor的hash不会变
-		new webpack.HashedModuleIdsPlugin({
-			hashFunction: 'sha256',
-			hashDigest: 'hex',
-			hashDigestLength: 20
-		})
-	],
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserJSPlugin(),
-			new OptimizeCSSAssetsPlugin()
-		],
-		splitChunks: {
-			chunks: 'all',
-			minSize: 10000, // 最小10kb的chunks
-			maxSize: 0,
-			minChunks: 1,
-			maxAsyncRequests: 5,
-			maxInitialRequests: 3,
-			automaticNameDelimiter: '-',
-			name: true,
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10
-				},
-				default: {
-					minChunks: 2,
-					priority: -20,
-					reuseExistingChunk: true
-				}
-			}
-		}
-	}
+    mode: 'production',
+    entry: Utils.getEntry(),
+    plugins: [
+        // 设置当前环境为production
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name].[hash].css',
+            chunkFilename: 'assets/css/[id].[hash].css',
+            hmr: Config.NODE_ENV === 'dev',
+            publicPath: Config.build.publicPath
+        }),
+        // 该插件会根据模块的相对路径生成一个四位数的hash作为模块id, 保证vendor的hash不会变
+        new webpack.HashedModuleIdsPlugin({
+            hashFunction: 'sha256',
+            hashDigest: 'hex',
+            hashDigestLength: 20
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserJSPlugin(),
+            new OptimizeCSSAssetsPlugin()
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 10000, // 最小10kb的chunks
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '-',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    }
 });
 
 // 是否开启bundle分析
 if (Config.build.bundleAnalyze) {
-	webpackConfig.plugins.push(
-		new WebpackBundleAnalyzer({
-			analyzerMode: 'static'
-		}),
-	);
+    webpackConfig.plugins.push(
+        new WebpackBundleAnalyzer({
+            analyzerMode: 'static'
+        }),
+    );
 }
 
 // 是否开启sourceMap，建议不开启
 if (Config.build.sourceMap) {
-	// sourcemap
-	webpackConfig.plugins.push(
-		new webpack.SourceMapDevToolPlugin({
-			filename: `${Config.build.sourceMapFolder}/[file].map`
-		}),
-	);
+    // sourcemap
+    webpackConfig.plugins.push(
+        new webpack.SourceMapDevToolPlugin({
+            filename: `${Config.build.sourceMapFolder}/[file].map`
+        }),
+    );
 }
 
 if (Config.build.gzipEnable) {
-	webpackConfig.plugins.push(
-		new CompressionPlugin({
-			test: new RegExp('\\.(' + Config.build.gzipExtensions.join('|') + ')$'),
-			filename: '[path].gz[query]',
-			threshold: 10240, // 10MB
-			algorithm: 'gzip'
-		})
-	);
+    webpackConfig.plugins.push(
+        new CompressionPlugin({
+            test: new RegExp('\\.(' + Config.build.gzipExtensions.join('|') + ')$'),
+            filename: '[path].gz[query]',
+            threshold: 10240, // 10MB
+            algorithm: 'gzip'
+        })
+    );
 }
 
 if (Config.build.performanceInfo) {
-	webpackConfig.performance = {
-		maxEntrypointSize: 300000, // 入口资源超过300kb，发出警告
-		maxAssetSize: 150000, // 单个资源超过150KB，发出警告
-	};
+    webpackConfig.performance = {
+        maxEntrypointSize: 300000, // 入口资源超过300kb，发出警告
+        maxAssetSize: 150000, // 单个资源超过150KB，发出警告
+    };
 }
 
 // 增加htmlwebpackplugins
