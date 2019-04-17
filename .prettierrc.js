17f6b4b617c871d2df243d034c52817a9c801f4a
SecretCastle <chenfangzhong@hotmail.com>
2019-04-17 09:02:17 +0800

    upgrade config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "tabWidth": 4,
+    "useTabs": false,
+    "semi": true,
+    "singleQuote": true,
+    "jsxSingleQuote": true,
+    "requirePragma": true,
+    "insertPragma": true
+}
