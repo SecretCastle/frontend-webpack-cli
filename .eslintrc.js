bc649550b6e9a78e0f0fc09e39de285d13778ad3
SecretCastle <chenfangzhong@hotmail.com>
2019-05-14 17:19:23 +0800

    fix http server problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
         "linebreak-style": [
             "error",
             "unix"
