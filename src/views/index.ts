9e41096e278bb86c16646e2f82afe4bee3001f82
SecretCastle <chenfangzhong@hotmail.com>
2019-05-31 15:09:09 +0800

    .
    upgrade ts config and sample

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,5 +1,8 @@
-// import style from './style.css';
-// import * as Logo from '../assets/images/logo.png';
+/// <reference path="./index.d.ts"/>
+
+import style from './style.css';
+import Logo from '@/assets/images/logo.png';
+
 const createSimDom = () => {
     const div = document.createElement('div');
     div.style.width = '400px';
@@ -9,14 +12,14 @@ const createSimDom = () => {
     div.style.transform = 'translate3d(-50%, -50%, 0)';
     const imgDom2 = new Image();
     imgDom2.style.width = '100%';
-    // imgDom2.src = Logo;
+    imgDom2.src = Logo;
     const p = document.createElement('p');
     p.innerHTML = 'webpack模版';
     p.style.textAlign = 'center';
     div.appendChild(imgDom2);
     div.appendChild(p);
     // div.innerHTML = 'hello world';
-    // div.classList.add(style.hello);
+    div.classList.add(style.hello);
     return div;
 };
 
