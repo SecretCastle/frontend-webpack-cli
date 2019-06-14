f4d9ccacfcecd66956a1cf445cabc37ae92a8649
SecretCastle <chenfangzhong@hotmail.com>
2019-06-14 17:14:50 +0800

    .
    规范化结构目录

--- config/index.js
+++ config/index.js
@@ -36,7 +36,7 @@ module.exports = {
     proxyUrl: '/',
     // 获取入口路径
     getEntry() {
-        return this.spa ? './src/views/index.js' : './src/views/**/*.js';
+        return this.spa ? './src/index.js' : './src/**/*.js';
     },
     cssModule: false, //是否开启css module
     build: {
